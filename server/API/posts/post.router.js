7652b8c6a2e477ec706b6f12baaf2f4a84c456e5
treyliebscher <wcliebsc@gmail.com>
2019-05-24 14:24:05 -0400

    fixed res for post creation + added simple GET endpoint for posts

--- server/API/posts/post.router.js
+++ server/API/posts/post.router.js
@@ -13,43 +13,72 @@ const router = express.Router();
 // POST - Create a post within a Hive \\
 async function createPost(req, res){
     const targetHive = await HiveModel.findOne({_id: req.body.hive});
-    
+
+    const memberValidation = targetHive.members.map((member) => {
+        if(member._id == req.body.user){
+            return true;
+        } else {
+            return false;
+        }
+    });
+
     if(targetHive === null){
         return res.json({
             message: 'The hive in which you are trying to post does not exist'
+        })
+    }
+
+    else if (memberValidation === false){
+        return res.json({
+            message: 'You must become a member of this Hive before you are allowed to post'
         });
     }
-    
+
     else {
-        targetHive.members.map((member) => {
-            if(member._id == req.body.user){
-                let newPost = new PostModel(req.body);
-                newPost.save();
-                targetHive.posts.push(newPost);
-                targetHive.save(function(err, hive) {
-                    UserModel.findById(req.body.user, function(err, user) {
-                        user.posts.push(newPost);
-                        user.save(function(err) {
-                            UserModel.findById(req.body.user)
-                            .populate('posts')
-                            .populate('hives')
-                            .exec(function (err, posts) {
-                                res.json({
-                                    userFeedback: posts.serialize()
-                                })
-                            })
-                        })
+        let newPost = new PostModel(req.body);
+        newPost.save();
+        UserModel.findById(req.body.user, function(err, user){
+            user.posts.push(newPost);
+            user.save();
+        });
+        HiveModel.findById(req.body.hive, function(err, hive){
+            hive.posts.push(newPost);
+            hive.save(function(err) {
+                HiveModel.findById(req.body.hive)
+                .populate('posts')
+                .exec(function(err, hive) {
+                    res.json({
+                        userFeedback: hive.serialize()
                     })
                 })
-            } else {
-                return res.json({
-                    message: 'You must become a member of this Hive before you are allowed to post'
-                })
-            }
+            })
         })
     }
 }
 
 router.post('/create', tryCatch(createPost));
+
+// GET - View a post \\
+async function viewPost(req, res){
+    const targetPost = PostModel.findOne({_id: req.body.post});
+
+    if(targetPost === null){
+        return res.json({
+            message: 'That post does not exist'
+        });
+    }
+
+    else {
+        PostModel.findById(req.body.post)
+        .populate('comments')
+        .exec(function (err, post) {
+            res.json({
+                feedback: post.serialize()
+            })
+        })
+    }
+}
+
+router.get('/view', tryCatch(viewPost));
 
 module.exports = router;
