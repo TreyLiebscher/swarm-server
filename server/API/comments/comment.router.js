3ad022988df1ca91ae30812a3a5df72800effd86
treyliebscher <wcliebsc@gmail.com>
2019-05-24 14:23:13 -0400

    set username to show up as author

--- server/API/comments/comment.router.js
+++ server/API/comments/comment.router.js
@@ -13,6 +13,7 @@ const router = express.Router();
 // POST - Create a comment within a post \\
 async function createComment(req, res){
     const targetPost = await PostModel.findOne({_id: req.body.post});
+    const commentAuthor = await UserModel.findOne({_id: req.body.user});
 
     if(targetPost === null){
         return res.json({
@@ -21,16 +22,24 @@ async function createComment(req, res){
     }
 
     else {
-        let newComment = new CommentModel(req.body);
+        let newComment = new CommentModel({
+            post: req.body.post,
+            user: req.body.user,
+            body: req.body.body,
+            author: commentAuthor.username
+        });
         newComment.save();
-        targetPost.comments.push(newComment);
-        targetPost.save();
         
-        PostModel.findById(req.body.post)
-        .populate('comments')
-        .exec(function(err, post) {
-            res.json({
-                userFeedback: post.serialize()
+        PostModel.findById(req.body.post, function(err, post){
+            post.comments.push(newComment);
+            post.save(function(err) {
+                PostModel.findById(req.body.post)
+                .populate('comments')
+                .exec(function(err, post) {
+                    res.json({
+                        userFeedback: post.serialize()
+                    })
+                })
             })
         })
     }
