85762fbad0d70bb26ef9877024a4cab7a7a267d4
treyliebscher <wcliebsc@gmail.com>
2019-05-24 12:19:55 -0400

    added POST endpoint for commentsRouter

--- server/API/comments/comment.router.js
+++ server/API/comments/comment.router.js
@@ -1,25 +1,41 @@
 const express = require('express');
 
-const CommentModel = require('./comment.model');
-
 const tryCatch = require('../../helpers').expressTryCatchWrapper;
 const getFields = require('../../helpers').getFieldsFromRequest;
 
-const passport = require('passport');
-
-const {
-    localStrategy,
-    jwtStrategy
-} = require('../../../auth/strategies');
-
-passport.use(localStrategy);
-passport.use(jwtStrategy);
-
-const jwtAuth = passport.authenticate('jwt', {
-    session: false
-});
+const { UserModel } = require('../users/user.model');
+const { HiveModel } = require('../hives/hive.model');
+const { PostModel } = require('../posts/post.model');
+const { CommentModel } = require('./comment.model');
 
 const router = express.Router();
 
-const COMMENT_MODEL_FIELDS = ['body'];
+// POST - Create a comment within a post \\
+async function createComment(req, res){
+    const targetPost = await PostModel.findOne({_id: req.body.post});
 
+    if(targetPost === null){
+        return res.json({
+            message: 'This post does not exist'
+        });
+    }
+
+    else {
+        let newComment = new CommentModel(req.body);
+        newComment.save();
+        targetPost.comments.push(newComment);
+        targetPost.save();
+        
+        PostModel.findById(req.body.post)
+        .populate('comments')
+        .exec(function(err, post) {
+            res.json({
+                userFeedback: post.serialize()
+            })
+        })
+    }
+}
+
+router.post('/create', tryCatch(createComment));
+
+module.exports = router;
